d4f077d7c49f3873066fec7e77cb3ab0b35fb8aa
dediprasetio <dediprasetio03@gmail.com>
2019-11-13 21:28:33 +0700

    get list user

--- application/controllers/Litbangyasa.php
+++ application/controllers/Litbangyasa.php
@@ -6,8 +6,7 @@ class Litbangyasa extends CI_Controller
 
     public function index()
     {
-        $data['user'] = $this->db->get_where('msuser', ['email' =>
-        $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->lapan_api_library->call3('users/getuserbyemail', ['token' => TOKEN, $this->session->userdata('email')]);
 
         $hasil_getisihalaman = $this->lapan_api_library->call('halaman/getisihalaman', ['token' => TOKEN, 'seo' => 'litbangyasa']);
         $data['lit'] = $hasil_getisihalaman['rows'][0];
